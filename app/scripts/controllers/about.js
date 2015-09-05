c15b40c3c57e485fdee2276be609fd5b310fd3c1
Jaspreet <jjuneja@beachbody.com>
2015-09-05 12:38:46 +0530

    brotherHood First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bhApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bhApp
+ */
+angular.module('bhApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
